521b3ffd941d84d17a0334145fd8342c18e560c0
DEV-meme220 <emersonsouzarodrigues92@gmail.com>
2025-06-03 21:25:47 -0300

    cartao de credito

---
+++ Projetos/Exemplos/Cartao de Credito/script.js
@@ -0,0 +1,4 @@
+ let botao = document.getElementById("clicar()")
+    botao.addEventListener("click", function clicar(){
+        alert("obrigado pelas infomaçoes meu chapa, aguarde notificaçoes do seu personagem no seu devido banco. ^-^")
+    })
