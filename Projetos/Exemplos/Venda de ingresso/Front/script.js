2a9d22742b4c1f5adb0e2747729e71fe51f79c0c
DEV-meme220 <emersonsouzarodrigues92@gmail.com>
2025-06-06 12:55:12 -0300

    body

---
+++ Projetos/Exemplos/Venda de ingresso/Front/script.js
@@ -0,0 +1,8 @@
+let continuar = document.getElementById("clicar()")
+    continuar.addEventListener("click", function clicar(){
+        window.location.href = "index3.html"
+    })
+ let login = document.getElementById("logar()")   
+    login.addEventListener("click", function logar(){
+        window.location.href = "index2.html"
+    })
