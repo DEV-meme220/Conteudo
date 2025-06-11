d55b9c4ad5aeee70a82beaeed359460b53eff26d
DEV-meme220 <emersonsouzarodrigues92@gmail.com>
2025-06-11 12:25:26 -0300

    script

--- Projetos/Exemplos/Venda de ingresso/Front/script.js
+++ Projetos/Exemplos/Venda de ingresso/Front/script.js
@@ -1,8 +1,6 @@
+let inpu = document.getElementById("sen")
 let continuar = document.getElementById("clicar()")
     continuar.addEventListener("click", function clicar(){
         window.location.href = "index3.html"
     })
- let login = document.getElementById("logar()")   
-    login.addEventListener("click", function logar(){
-        window.location.href = "index2.html"
-    })
+    
